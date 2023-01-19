822d0561c53bf556998ff380bff0de790e8e7ee0
Manav Chhuneja <manavchhuneja64@gmail.com>
2023-01-19 16:40:13 -0500

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Manav Chhuneja on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
